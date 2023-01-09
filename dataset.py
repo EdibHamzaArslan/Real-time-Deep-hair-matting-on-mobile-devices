ab4c2d9adee03c50458450d4323c6b9e8f7603ca
EdibHamzaArslan <claymorehack@gmail.com>
2023-01-10 01:25:24 +0300

    dataset.py's tests are removed.

--- dataset.py
+++ dataset.py
@@ -1,12 +1,8 @@
 import os
-# import cv2
-# import numpy as np
-# import matplotlib.pyplot as plt
 from PIL import Image
 
 import torch
 import torchvision.transforms as T
-
 
 
 class LFW(torch.utils.data.Dataset):
@@ -32,8 +28,6 @@ class LFW(torch.utils.data.Dataset):
             exit(-1)
         
         self.create_mode_paths()
-        
-
         self.mode_mask_paths = [os.path.join(self.root_masks, (os.path.split(img_path)[-1].split(".")[0] + ".ppm"))  for img_path in self.mode_img_paths]
     
     def create_mode_paths(self):
@@ -47,10 +41,8 @@ class LFW(torch.utils.data.Dataset):
             out = os.path.join(sub_path, (name + "_" + (self.max_number_rigid - len(str(int(number)))) * "0" + str(int(number)) + ".jpg"))
             self.mode_img_paths.append(out)
 
-    
     def __len__(self):
         return len(self.mode_mask_paths)
-
     
     def __getitem__(self, index):
         img_path = self.mode_img_paths[index]
@@ -67,29 +59,3 @@ class LFW(torch.utils.data.Dataset):
             mask = self.transform(mask)
         
         return img, mask
-
-'''
-if __name__ == "__main__":
-    transform = T.Compose(
-        [
-            T.Resize((224, 224)),
-            T.ToTensor(),
-        ]
-    )
-    dataset = LFW("data/lfw_data_imgs", "data/lfw_hair_masks", mode="train", transform=transform, debug=True)
-    train_loader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True)
-    
-    #### TEST
-    # for idx, (imgs, masks) in enumerate(train_loader):
-    #     print(imgs.shape)
-    #     print(masks.shape)
-        
-    #     # img = cv2.cvtColor(np.array(imgs[0].permute(1, 2, 0)), cv2.COLOR_RGB2BGR)
-    #     # mask = cv2.cvtColor(np.array(masks[0].permute(1, 2, 0)), cv2.COLOR_RGB2BGR)
-    #     # cv2.imshow("img", img)
-    #     # cv2.waitKey(0)
-    #     # cv2.imshow("mask", mask)
-    #     # cv2.waitKey(0)
-        
-    #     break
-'''
