d24e7cebc5060b126f983b658a80b2cabb1a0bb9
EdibHamzaArslan <claymorehack@gmail.com>
2023-01-10 03:33:55 +0300

    model.py updated

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 class DepthWiseEncoder(nn.Module):
     def __init__(self, in_channels, out_channels, stride, padding):
         super(DepthWiseEncoder, self).__init__()
-        self.block = nn.Sequential(
+        self.conv_block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=padding, groups=in_channels, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
@@ -15,35 +15,41 @@ class DepthWiseEncoder(nn.Module):
             nn.ReLU(inplace=True),
         )
     def forward(self, x):
-        return self.block(x)
+        return self.conv_block(x)
 
 
 class DepthWiseDecoder(nn.Module):
     def __init__(self, in_channels, out_channels, stride, padding):
         super(DepthWiseDecoder, self).__init__()
-        self.block = nn.Sequential(
+        self.conv_block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=padding, groups=out_channels),
             nn.Conv2d(out_channels, out_channels, kernel_size=1),
             nn.ReLU(inplace=True),
         )
     
     def forward(self, x):
-        return self.block(x)
+        return self.conv_block(x)
 
 
 
 class Encoder(nn.Module):
     def __init__(self, in_channels=32, out_channels_list=[64, 128, 256, 512, 1024]):
         super(Encoder, self).__init__()
-        self.encoder = nn.Sequential(
+        self.encoder_layer1 = nn.Sequential(
             # 64
             DepthWiseEncoder(in_channels, out_channels_list[0], stride=1, padding=1),
+        )
+        self.encoder_layer2 = nn.Sequential(
             # 128
             DepthWiseEncoder(out_channels_list[0], out_channels_list[1], stride=2, padding=1),
             DepthWiseEncoder(out_channels_list[1], out_channels_list[1], stride=1, padding=1),
+        )
+        self.encoder_layer3 = nn.Sequential(
             # 256
             DepthWiseEncoder(out_channels_list[1], out_channels_list[2], stride=2, padding=1),
             DepthWiseEncoder(out_channels_list[2], out_channels_list[2], stride=2, padding=1),
+        )
+        self.encoder_layer4 = nn.Sequential(
             # 512
             DepthWiseEncoder(out_channels_list[2], out_channels_list[3], stride=1, padding=1),
             DepthWiseEncoder(out_channels_list[3], out_channels_list[3], stride=1, padding=1),
@@ -51,30 +57,37 @@ class Encoder(nn.Module):
             DepthWiseEncoder(out_channels_list[3], out_channels_list[3], stride=1, padding=1),
             DepthWiseEncoder(out_channels_list[3], out_channels_list[3], stride=1, padding=1),
             DepthWiseEncoder(out_channels_list[3], out_channels_list[3], stride=2, padding=1),
+        )
+        self.encoder_layer5 = nn.Sequential(
             # 1024
             DepthWiseEncoder(out_channels_list[3], out_channels_list[4], stride=1, padding=1),
             DepthWiseEncoder(out_channels_list[4], out_channels_list[4], stride=1, padding=1),
         )
     
     def forward(self, x):
-        return self.encoder(x)
+        x_res1 = self.encoder_layer1(x)
+        x_res2 = self.encoder_layer2(x_res1)
+        x_res3 = self.encoder_layer3(x_res2)
+        x_res4 = self.encoder_layer4(x_res3)
+        x = self.encoder_layer5(x_res4)
+        return x, x_res1, x_res2, x_res3, x_res4
 
 
 class Decoder(nn.Module):
     def __init__(self, in_channels=1024, out_channels_list=[64, 64, 64, 64, 64]):
         super(Decoder, self).__init__()
-        self.decoder_layer1 = DepthWiseDecoder(in_channels, out_channels_list[0], stride=1, padding=1)
-        self.decoder_layer2 = DepthWiseDecoder(out_channels_list[0], out_channels_list[1], stride=1, padding=1)
-        self.decoder_layer3 = DepthWiseDecoder(out_channels_list[1], out_channels_list[2], stride=1, padding=1)
-        self.decoder_layer4 = DepthWiseDecoder(out_channels_list[2], out_channels_list[3], stride=1, padding=1)
-        self.decoder_layer5 = DepthWiseDecoder(out_channels_list[3], out_channels_list[4], stride=1, padding=1)
+        self.decoder_block1 = DepthWiseDecoder(in_channels, out_channels_list[0], stride=1, padding=1)
+        self.decoder_block2 = DepthWiseDecoder(out_channels_list[0], out_channels_list[1], stride=1, padding=1)
+        self.decoder_block3 = DepthWiseDecoder(out_channels_list[1], out_channels_list[2], stride=1, padding=1)
+        self.decoder_block4 = DepthWiseDecoder(out_channels_list[2], out_channels_list[3], stride=1, padding=1)
+        self.decoder_block5 = DepthWiseDecoder(out_channels_list[3], out_channels_list[4], stride=1, padding=1)
     
     def forward(self, x):
-        out1 = self.decoder_layer1(F.interpolate(x, scale_factor=2, mode="nearest"))
-        out2 = self.decoder_layer2(F.interpolate(out1, scale_factor=2, mode="nearest"))
-        out3 = self.decoder_layer3(F.interpolate(out2, scale_factor=2, mode="nearest"))
-        out4 = self.decoder_layer3(F.interpolate(out3, scale_factor=2, mode="nearest"))
-        out5 = self.decoder_layer3(F.interpolate(out4, scale_factor=2, mode="nearest"))
+        out1 = self.decoder_block1(F.interpolate(x, scale_factor=2, mode="nearest"))
+        out2 = self.decoder_block2(F.interpolate(out1, scale_factor=2, mode="nearest"))
+        out3 = self.decoder_block3(F.interpolate(out2, scale_factor=2, mode="nearest"))
+        out4 = self.decoder_block4(F.interpolate(out3, scale_factor=2, mode="nearest"))
+        out5 = self.decoder_block5(F.interpolate(out4, scale_factor=2, mode="nearest"))
         return out5
 
 
@@ -83,13 +96,18 @@ class HairSegNet(nn.Module):
     def __init__(self, in_channels, num_classes):
         super(HairSegNet, self).__init__()
         self.first_conv = nn.Conv2d(in_channels, 32, kernel_size=3, stride=2, padding=1)
-        self.final_conv = nn.Conv2d(64, num_classes, kernel_size=1)
         self.encoder = Encoder(in_channels=32, out_channels_list=[64, 128, 256, 512, 1024])
+        self.res_conv1
+        self.res_conv2
+        self.res_conv3
+        self.res_conv4 = nn.Conv2d() # TODO: continue from here.
         self.decoder = Decoder(in_channels=1024, out_channels_list=[64, 64, 64, 64, 64])
+        self.final_conv = nn.Conv2d(64, num_classes, kernel_size=1)
     
     def forward(self, x):
         x = self.first_conv(x)
-        x = self.encoder(x)
+        x, encoder_res1, encoder_res2, encoder_res3, encoder_res4 = self.encoder(x)
+
         x = self.decoder(x)
         return self.final_conv(x)
     
@@ -118,6 +136,8 @@ if __name__ == "__main__":
 
     x = torch.randn(1, 3, 224, 224)
     model = HairSegNet(3, 2)
+    print(model)
+    # print(model.encoder.encoder_block[0].block[3])
     out = model(x)
     print(out.shape)
     assert out.shape == torch.Size([1, 2, 224, 224]), "Test Failed!"
